61a1f9431090c33e9066cbb2b09db5cd99e61ffd
jonex41 <agudajohn5@gmail.com>
2022-07-26 17:17:49 +0100

    feat: :zap: made some changes on splash screen

--- lib/pages/splash/splash_screen.dart
+++ lib/pages/splash/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_shopper/gen/assets.gen.dart';
 import 'package:flutter_shopper/pages/onboard_page/view/first_screen.dart';
@@ -14,9 +15,10 @@ class SplashScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     Future<void> _afterSplash() async {
+      await 2.seconds.delay;
       context.replaceRoute(const FirstRoute());
       //await 1.seconds.delay;
-     /*  bool isLoggedIn = getBoolAsync(kHasAccount, defaultValue: false);
+      /*  bool isLoggedIn = getBoolAsync(kHasAccount, defaultValue: false);
       if (isLoggedIn) {
         
       context.replaceRoute(TabRoute());
@@ -29,13 +31,28 @@ class SplashScreen extends HookWidget {
     useEffect(() {
       _afterSplash();
     });
-    return Scaffold(
-        body: Center(
-      child:SvgPicture.asset(
-          "assets/images/logo.svg",
-          height: context.height() * 0.25,
-        ), /* Assets.images.logo
-          .image(width: context.width() - 164), */
-    ));
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.white,
+
+        // For Android.
+        // Use [light] for white status bar and [dark] for black status bar.
+        // statusBarIconBrightness: Brightness.dark,
+        // For iOS.
+        // Use [dark] for white status bar and [light] for black status bar.
+        // statusBarBrightness: Brightness.dark,
+      ),
+      child: Scaffold(
+          body: Container(
+        color: Color(0xFF01221D),
+        child: Center(
+          child: SvgPicture.asset(
+            "assets/images/logo.svg",
+            height: context.height() * 0.25,
+          ), /* Assets.images.logo
+              .image(width: context.width() - 164), */
+        ),
+      )),
+    );
   }
 }
